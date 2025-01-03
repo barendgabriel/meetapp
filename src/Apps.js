48099f4745ab08971e2ed00792be0e6ea2a59456
barendgabriel <jansenvanrensburg.ben@gmail.com>
2025-01-03 22:14:22 +0100

    Update Apps.js

--- src/Apps.js
+++ src/Apps.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Optional: You can add a dynamic greeting with the current time
+  const hour = new Date().getHours();
+  let greeting;
+
+  if (hour < 12) {
+    greeting = 'Good Morning!';
+  } else if (hour < 18) {
+    greeting = 'Good Afternoon!';
+  } else {
+    greeting = 'Good Evening!';
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* You can replace the logo and other content */}
+        <h1>{greeting}</h1> {/* Display the greeting */}
+        <p>Welcome to my React app!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
